b947377bd490c97c3736a93e231c4759d4b03da6
DziubaAV <dz.artsiomi@gmail.com>
2022-12-15 10:47:14 +0300

    Закладка Trener

--- resources/views/trener_page.blade.php
+++ resources/views/trener_page.blade.php
@@ -1,14 +1,65 @@
 @extends('layouts.app')
-
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Тренерский состав</div> 
-
-                <div class="card-body">
-                  Тренера
+                    <div class="trener-body">
+                <!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+                                <div class='trener-table'>
+                                <h2 style="text-align:center">Городилов Игорь Борисович</h2>
+                                <div class="trener-card">
+                                    <img src="..\images\Gorodilov_IB.jpg" alt="Городилов Игорь Борисович" style="width:90%">
+                                    <h3>Директор</h3>
+                                    <!-- <p class="trener-title">CEO & Founder, Example</p> -->
+                                    <!-- <p>Harvard University</p> -->
+                                        <div style="margin: 24px 0;">
+                                            <!-- <a href="#"><i class="fa fa-dribbble"></i></a> -->
+                                            <!-- <a href="#"><i class="fa fa-twitter"></i></a> -->
+                                            <!-- <a href="#"><i class="fa fa-linkedin"></i></a>   -->
+                                            <a href="https://www.facebook.com/profile.php?id=100023557853404" target="_blank"><i class="fa fa-facebook"></i></a> 
+                                        </div>
+                                    <p><button>Позвонить</button></p>
+                                </div>
+                                </div>
+                                
+                <!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+                                <div class='trener-table'>
+                                <!-- <h2 style="text-align:center">Директор</h2> -->
+                                <div class="trener-card">
+                                    <img src="#" alt="#" style="width:100%">
+                                    <h2>Старший тренер</h2>
+                                    <!-- <p class="trener-title">CEO & Founder, Example</p> -->
+                                    <!-- <p>Harvard University</p> -->
+                                        <div style="margin: 24px 0;">
+                                            <!-- <a href="#"><i class="fa fa-dribbble"></i></a> -->
+                                            <!-- <a href="#"><i class="fa fa-twitter"></i></a> -->
+                                            <!-- <a href="#"><i class="fa fa-linkedin"></i></a>   -->
+                                            <!-- <a href="#"><i class="fa fa-facebook"></i></a> -->
+                                        </div>
+                                    <!-- <p><button>Contact</button></p> -->
+                                </div>
+                                </div>
+                <!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+                                <div class='trener-table'>
+                                <!-- <h2 style="text-align:center">Директор</h2> -->
+                                <div class="trener-card">
+                                    <img src="#" alt="#" style="width:100%">
+                                    <h2>Старший тренер</h2>
+                                    <!-- <p class="trener-title">CEO & Founder, Example</p> -->
+                                    <!-- <p>Harvard University</p> -->
+                                        <div style="margin: 24px 0;">
+                                            <!-- <a href="#"><i class="fa fa-dribbble"></i></a> -->
+                                            <!-- <a href="#"><i class="fa fa-twitter"></i></a> -->
+                                            <!-- <a href="#"><i class="fa fa-linkedin"></i></a>   -->
+                                            <!-- <a href="#"><i class="fa fa-facebook"></i></a> -->
+                                        </div>
+                                    <!-- <p><button>Contact</button></p> -->
+                                </div>
+                                </div>
+                <!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
                 </div>
             </div>
         </div>
