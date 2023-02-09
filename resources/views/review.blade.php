814f41f2a641caeb229b2c5205c5a64129bc982a
DziubaAV <dz.artsiomi@gmail.com>
2023-02-09 23:53:06 +0300

    Ото

--- resources/views/review.blade.php
+++ resources/views/review.blade.php
@@ -13,22 +13,16 @@
                         
                         
                         <b>{{ $review->users->name }}: </b>
-                        {{ $review->body }}
-
+                        {{ $review->body }}                      
     
                         
                         <div class="fl_right">
+                        <div class="rating-mini">                               
+                                    <span type="$stars" class="active">{{ $review->stars }}</span>	  
+                                </div>  
                             <div>
                                 {{ $review->created_at }}
-                            </div>
-
-                                <div class="rating-mini">
-                                    <span class="active"></span>	
-                                    <span class="active"></span>    
-                                    <span class="active"></span>  
-                                    <span></span>    
-                                    <span></span>
-                                </div>   
+                            </div> 
                         </div>
 
            
@@ -50,15 +44,15 @@
 
                             <div class="rating-area">
                                 <input type="radio" id="star-5" name="stars" value="5">
-                                <label for="star-5" title="Оценка '5'"></label>	
-                                <input type="radio" id="star-4" name="stars" value="4">
-                                <label for="star-4" title="Оценка '4'"></label>    
+                                <label for="star-5" title="5 баллов"></label>	
+                                <input type="radio" id="star-4" name="stars" value="4" checked="checked">
+                                <label for="star-4" title="4 балла"></label>    
                                 <input type="radio" id="star-3" name="stars" value="3">
-                                <label for="star-3" title="Оценка '3'"></label>  
+                                <label for="star-3" title="3 балла"></label>  
                                 <input type="radio" id="star-2" name="stars" value="2">
-                                <label for="star-2" title="Оценка '2'"></label>    
+                                <label for="star-2" title="2 балла"></label>    
                                 <input type="radio" id="star-1" name="stars" value="1">
-                                <label for="star-1" title="Оценка '1'"></label>
+                                <label for="star-1" title="1 балл"></label>
                             </div>
                            
                             <textarea placeholder="Текст отзыва..." name="body" class="form-control" id="exampleFormControlTextarea1" rows="3" required></textarea>
